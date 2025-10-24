fab092697c7c50ba840454c0703fbfd00e9b57a0
DeenaElOrra <deenaeo@al.insper.edu.br>
2025-10-24 20:36:35 -0300

    product service

---
+++ src/main/java/store/product/ProductApplication.java
@@ -0,0 +1,16 @@
+package store.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableCaching
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+}
